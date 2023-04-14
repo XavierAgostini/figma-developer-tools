dd43a0fbc68ad702b54d868f4ad0645d608e75b9
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2023-04-14 12:54:35 -0500

    add tooltip to search

--- src/ui/views/Search/index.tsx
+++ src/ui/views/Search/index.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import classNames from 'classnames'
 import SearchFilterDropdown from '../../components/SearchFilterDropdown'
 import FigmaPageList from '../../components/FigmaPageList'
-import style from './style.module.css'
-import { Select, Input, Title, Text, Button } from "react-figma-plugin-ds";
+import { Tooltip } from 'react-tooltip'
+
+
+import 'react-tooltip/dist/react-tooltip.css'
+
+import { Select, Title, Text, Button } from "react-figma-plugin-ds";
 import { SelectedItemsListProvider  } from "../../context/SelectedItemsList"
 import { useFigmaSearch } from './useFigmaSearch'
 import SearchInput from '../../components/SearchInput'
+
+import style from './style.module.css'
+import { FigmaHelpIcon } from '../../components/FigmaIcons'
+
 const PAGE_FILTER_LIST = [
   { id: 'CURRENT', name: 'This Page'},
   { id: 'ALL', name: 'All Pages'},
@@ -45,6 +53,35 @@ const Search = () => {
           setSelectedFilterOptions={setSelectedFilterOptions}
           resultsByNodeType={resultsByNodeType}
         />
+        <div className={style.tooltipWrapper}>
+          <FigmaHelpIcon className={style.helpIcon} data-tooltip-id="search-help" />
+          <Tooltip id="search-help" place="bottom" className={style.tooltip} >
+            <div className={style.tooltipContent}>
+              <div className={style.tooltipGroup}>
+                <div className={style.tooltipTitle}>Search</div>
+                <div className={style.tooltipText}>
+                  <ul>
+                    <li>Layer Name</li>
+                    <li>Text Value</li>
+                    <li>
+                      <div>Figma Node Id: </div>
+                      <div>i.e "id="0:1"</div>
+                      </li>
+                  </ul>
+              </div>
+              <div className={style.tooltipGroup}>
+                <div className={style.tooltipTitle}>Filter</div>
+                <div className={style.tooltipText}>
+                  <ul>
+                    <li>Layer Type</li>
+                    <li>Page</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            </div>
+          </Tooltip>
+        </div>
       </div>
       <div className={style.body}>
         <div className={style.pageResultsInfo}>
